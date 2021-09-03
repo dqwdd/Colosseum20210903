0b53fe8c0a276b475b6d1e14da156566c77217f7
keemaan <keemaan@naver.com>
2021-09-03 15:42:26 +0900

    서버 통신 전담 클래스인 ServerUtil 생성 - companion object {} 함수/변수 작성 예정

---
+++ app/src/main/java/com/neppplus/colosseum20210903/utils/ServerUtil.kt
@@ -0,0 +1,18 @@
+package com.neppplus.colosseum20210903.utils
+
+class ServerUtil {
+
+//    단순 기능 수행이란? == 서버에 요청을 날리고 -> 응답을 화면에 전달
+//    이 짓은 어떤 객체가 하던 요청/응답 처리만 잘 되면 그만임
+//    이런 함수를 만든다? -> 뭐로 만드는게 낫다? -> static 함수들로 활용. ServerUtil 기능() 코드 작성 가능
+
+    companion object {
+
+//        이 안에 만드는 변수 / 함수는 전부 static처럼 동작함
+
+
+    }
+
+
+
+}
