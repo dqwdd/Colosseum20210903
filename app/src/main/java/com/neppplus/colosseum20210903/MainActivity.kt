6edf141a087070647400979d209b0de9f99b3358
keemaan <keemaan@naver.com>
2021-09-03 16:38:31 +0900

    서버 응답 본문을 JSONObject를 거쳐서 로그 출력

--- app/src/main/java/com/neppplus/colosseum20210903/MainActivity.kt
+++ app/src/main/java/com/neppplus/colosseum20210903/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.colosseum20210903
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.colosseum20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -20,7 +21,11 @@ class MainActivity : BaseActivity() {
             val inputId = emailEdt.text.toString()
             val inputPw = passwordEdt.text.toString()
 
+
+
 //            서버에 이 데이터가 회원이 맞는지 확인 요청 => 로그인 시도
+
+            ServerUtil.postRequestSignIn(inputId, inputPw)
 
 
 
--- app/src/main/java/com/neppplus/colosseum20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/colosseum20210903/utils/ServerUtil.kt
@@ -1,8 +1,9 @@
 package com.neppplus.colosseum20210903.utils
 
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
 
 class ServerUtil {
 
@@ -46,8 +47,37 @@ class ServerUtil {
             
             val client = OkHttpClient()
             
-//            만들어진 요청 호출
-            client.newCall(request)//request를 호출해주세요하는거
+//            만들어진 요청 호출 -> 응답이 왔을 때 분석 / UI 반영
+//            호출을 하면 -> 서버가 데이터 처리...-> 응답 받아서 처리 (처리할 코드를 등록)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+//                    실패? 서버 연결 자체를 실패. 응답이 돌아오지 않은 경우에
+//                    ex) 비번 틀려서 로그인 실패 : 응답은 돌아왔는데 그 내용이 실패( 응답 O )
+//                    인터넷 끊김, 서버 접속 불가 등. 실패 O
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+//                    어떤 내용이든, 응답이 돌아온 경우(로그인 성공, 실패 모두 응답)
+//                    응답에 포함된 데이터들 중 -> 본문을 보자
+
+                    val bodyString = response.body!!.string()//자동완성때 보니까 body에 ?있어서 !! 붙임
+//                    본문을 그냥 String으로 찍어보면 -> 한글이 깨져보임
+//                    JSONObject 형태로 변환해서 -> 다시 String으로 바꿔보면 한글이 보임
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("서버 응답 본문", jsonObj.toString())
+
+
+
+                }
+
+
+            })
+        //request를 호출해주세요하는거
+            //enqueue()==호출을 하고 돌아올 때 할 일
             
             
         }
