25a69fd20bf9385b6febe8205ee222c36bb7dce6
keemaan <keemaan@naver.com>
2021-09-03 16:16:04 +0900

    ServerUtil.kt에 로그인 수행 함수 작업중 - 호출까지는 작업 완료 - 응답 받는 처리 아직 x

--- app/src/main/java/com/neppplus/colosseum20210903/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/colosseum20210903/utils/ServerUtil.kt
@@ -1,5 +1,9 @@
 package com.neppplus.colosseum20210903.utils
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class ServerUtil {
 
 //    단순 기능 수행이란? == 서버에 요청을 날리고 -> 응답을 화면에 전달
@@ -10,8 +14,44 @@ class ServerUtil {
 
 //        이 안에 만드는 변수 / 함수는 전부 static처럼 동작함
 
+//        호스트 주소를 애초에 변수로 저장해두자(가져다 쓰기 편하게 - ServerUtil안에서만)
+        private val HOST_URL = "http://52.180.52.26"
+
+
+//        로그인 기능 실행 함수
+
+        fun postRequestSignIn( id : String, pw: String ) {
+            
+//            1. 어디로(url) 갈 것인가?   HOST_URL + Endpoint
+            
+            val urlString = "${HOST_URL}/user"
+            
+//            2. 어떤 데이터를 가지고 갈 것인가? (파라미터)
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .build()
+        
+        
+        //    3. 어떤 방식으로 접근할 것인가? Request에 같이 적어주자
+        //    모두 모아서 하나의 Request 정보로 만들어주자
+            
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)//짐을 넣을거면 여기다 넣어라
+                .build()
+            
+//            만들어진 request를 실제로 호출해야함
+//            요청을 한다 -> 클라이언트의 역할 -> 앱이 클라이언트로 동작해야함
+            
+            val client = OkHttpClient()
+            
+//            만들어진 요청 호출
+            client.newCall(request)//request를 호출해주세요하는거
+            
+            
+        }
+
     }
-
-
 
 }
