62d9d6da56813097a1153600070bf003dc8b76e3
keemaan <keemaan@naver.com>
2021-09-03 15:20:04 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/colosseum20210903/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.colosseum20210903
+
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    lateinit var mContext : Context
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        mContext = this
+    }
+
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
